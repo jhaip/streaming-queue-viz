acdddb9ec327ef9dc4636b52d22a10bdad14a79f
Jacob Haip <haipjacob@gmail.com>
2018-01-21 13:40:47 -0500

    move data views to separate components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ListItem from './ListItem'
 import CodeListItem from './CodeListItem'
 import LineDiff from 'line-diff';
 import Timeline from './Timeline';
+import SerialDataView from './DataViews/SerialDataView'
+import CodeDataView from './DataViews/CodeDataView'
 
 class App extends Component {
   constructor(props) {
@@ -15,13 +16,6 @@ class App extends Component {
     };
     this.onTimelineSelection = this.onTimelineSelection.bind(this);
   }
-  scrollToBottom(source) {
-    if (source == "code") {
-      this.codeMessagesEnd.scrollIntoView();
-    } else {
-      this.serialMessagesEnd.scrollIntoView();
-    }
-  }
   onTimelineSelection(d) {
     this.setState({
       start: (typeof d.start !== 'undefined') ? d.start : this.state.start,
@@ -29,30 +23,9 @@ class App extends Component {
     })
   }
   componentDidMount() {
-    this.scrollToBottom("code");
-    this.scrollToBottom("serial");
     window.LineDiff = LineDiff;
   }
-  componentDidUpdate() {
-    this.scrollToBottom("code");
-    this.scrollToBottom("serial");
-  }
   render() {
-    const serialListItems = ((this.props.list || [])["serial"] || []).filter(x =>
-      (this.state.start === null || new Date(x.timestamp) >= this.state.start) &&
-      (this.state.end === null || new Date(x.timestamp) < this.state.end)
-    ).map(x =>
-      <ListItem datum={x} />
-    );
-    const codeListItems = ((this.props.list || [])["code"] || []).filter(x =>
-      (this.state.start === null || new Date(x.timestamp) >= this.state.start) &&
-      (this.state.end === null || new Date(x.timestamp) < this.state.end)
-    ).map((x, i, a) =>
-      <CodeListItem
-        datum={x}
-        prevDatum={(i >= 1) ? a[i-1] : null}
-      />
-    );
     return (
       <div className="App">
         <Timeline
@@ -62,18 +35,16 @@ class App extends Component {
           end={this.state.end}
         />
         <div style={{display: 'flex'}}>
-          <div className="ScrollContainer">
-            {serialListItems}
-            <div style={{ float:"left", clear: "both" }}
-                 ref={(el) => { this.serialMessagesEnd = el; }}>
-            </div>
-          </div>
-          <div className="ScrollContainer">
-            {codeListItems}
-            <div style={{ float:"left", clear: "both" }}
-                 ref={(el) => { this.codeMessagesEnd = el; }}>
-            </div>
-          </div>
+          <SerialDataView
+            data={((this.props.list || [])["serial"] || [])}
+            start={this.state.start}
+            end={this.state.end}
+          />
+          <CodeDataView
+            data={((this.props.list || [])["code"] || [])}
+            start={this.state.start}
+            end={this.state.end}
+          />
         </div>
       </div>
     );
---
+++ src/DataViews/CodeDataView.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import '../App.css';
+import CodeListItem from '../CodeListItem'
+
+class SerialDataView extends Component {
+  scrollToBottom() {
+    this.messagesEnd.scrollIntoView();
+  }
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+  render() {
+    const listItems = this.props.data.filter(x =>
+      (this.props.start === null || new Date(x.timestamp) >= this.props.start) &&
+      (this.props.end === null || new Date(x.timestamp) < this.props.end)
+    ).map((x, i, a) =>
+      <CodeListItem
+        datum={x}
+        prevDatum={(i >= 1) ? a[i-1] : null}
+      />
+    );
+    return (
+      <div className="ScrollContainer" key="code">
+        {listItems}
+        <div style={{ float:"left", clear: "both" }}
+             ref={(el) => { this.messagesEnd = el; }}>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SerialDataView;
