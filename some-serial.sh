ba1a445cc103f44c894495b01546fbd8490a25dc
Jacob Haip <haipjacob@gmail.com>
2018-02-09 19:24:52 -0500

    add a 50 ms delay in sending test serial messages

--- some-serial.sh
+++ some-serial.sh
@@ -1,4 +1,5 @@
 for run in {1..10}
 do
+    sleep 0.05
     curl -H "Content-Type: application/json" -X POST -d '{"source": "serial", "value": "'"$(( ( RANDOM % 10 )  + 1 ))"'"}' "http://localhost:8002/"
 done
