6f6a65aadf186293463f9fb6d323dc0cf368bbdc
Jacob Haip <haipjacob@gmail.com>
2018-02-13 21:00:17 -0500

    example select dropdown with styles

--- src/DataViews/DerivativeDataView.js
+++ src/DataViews/DerivativeDataView.js
@@ -211,6 +211,12 @@ class DerivativeDataView extends Component {
             >
               <i className="material-icons">code</i>
             </button>
+            <select className="menuDropdown">
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
           </div>
           { this.state.showCodeEditor &&
             <CodeMirror
